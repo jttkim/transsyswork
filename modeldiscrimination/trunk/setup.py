ed0815d71124625302d6f385b537c85844056eca
bkx08wju <anyelacamargo@users.noreply.github.com>
2010-03-23 19:05:07 +0000

    Setup file added

---
+++ modeldiscrimination/trunk/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+setup(name='trsysmodis',
+      version='1.0',
+      description='Transsys Model Discrimination Distribution Utilities',
+      author='Anyela Camargo, J. T Kim',
+      author_email='a.camargo-rodriguez@uea.ac.uk',
+      py_modules=['trsysmodis'],
+      scripts=['transsyscreatedata', 'transsyswritesimset', 'transsysrandomprogram', 'transsysreparam', 'transsysrewire'],
+      )
