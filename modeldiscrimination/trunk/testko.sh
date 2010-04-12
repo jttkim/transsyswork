40ecb44ec7e1fbec13a507b2624ca95a11d333d0
bkx08wju <anyelacamargo@users.noreply.github.com>
2010-04-12 16:11:28 +0000

    testko.sh reversed to original trsysmodis.py: Implementing knockoutTreatment, tested jinjutspec.txt: Language has been enriched

--- modeldiscrimination/trunk/testko.sh
+++ modeldiscrimination/trunk/testko.sh
@@ -18,11 +18,11 @@ do_run ./transsysreparam -T transformerfile.dat -s 1 -p 1 -n test test_targettop
 do_run mv test01.tra test_target.tra
 do_run ./transsyswritesimset -s 2 -e 100 -N 0.0 test_target.tra test_target
 do_run ./transsysrewire -w 1 -n test_candidate -r 1 -s 3 test_targettopology.tra 
-do_run ./transsysrewire -w 13 -n test_candidate -r 1 -s 3 test_targettopology.tra
-do_run ./transsysrewire -w 66 -n test_candidate -r 1 -s 3 test_targettopology.tra
+do_run ./transsysrewire -w 10 -n test_candidate -r 1 -s 3 test_targettopology.tra
+do_run ./transsysrewire -w 99 -n test_candidate -r 1 -s 3 test_targettopology.tra
 
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_targettopology -g optspec.dat -T transformerfile.dat -u correlation -L test_rew0 testlog_rew0.txt testfinalparam_rew0.txt
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_candidate_w01_r00 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_candidate_w10_r00 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_candidate_w90_r00 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
+do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_targettopology -g optspec.dat -T transformerfile.dat -u correlation -L test_rew0 testlog_rew0.txt testfinalparam_rew0.txt
+do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_candidate_w01_r00 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
+do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_candidate_w10_r00 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
+do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_candidate_w90_r00 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
 
