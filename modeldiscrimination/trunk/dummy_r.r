d32867a7ff549cba3456c98d4948150718c681cc
jtk <jtk@7efb0120-2ad2-4840-852c-56c2cfac7ca6>
2010-07-07 15:30:09 +0000

    partial model development

--- modeldiscrimination/trunk/dummy_r.r
+++ modeldiscrimination/trunk/dummy_r.r
@@ -24,3 +24,108 @@ plotjasmonate <- function(filename)
   }
 }
 
+
+plotProfiles <- function(d)
+{
+  for (n in rownames(d))
+  {
+    p <- as.numeric(d[n, ]);
+    names(p) <- colnames(d);
+    barplot(p, main = n);
+    if (readline(sprintf("%s -- q to quit: ", n)) == "q")
+    {
+      break;
+    }
+  }
+}
+
+
+traceColors <- function(nColors)
+{
+  n <- as.integer(ceiling(nColors / 3));
+  s <- 1.0 / n;
+  tc <- character();
+  for (i in 0:(n - 1))
+  {
+    r <- as.integer(round((1.0 - i * s) * 255));
+    g <- as.integer(round(i * s * 255));
+    b <- as.integer(0);
+    tc <- c(tc, sprintf("#%02x%02x%02x", r, g, b));
+    r <- as.integer(0);
+    g <- as.integer(round((1.0 - i * s) * 255));
+    b <- as.integer(round(i * s * 255));
+    tc <- c(tc, sprintf("#%02x%02x%02x", r, g, b));
+    r <- as.integer(round(i * s * 255));
+    g <- as.integer(0);
+    b <- as.integer(round((1.0 - i * s) * 255));
+    tc <- c(tc, sprintf("#%02x%02x%02x", r, g, b));
+  }
+  return(tc[1:nColors]);
+}
+
+
+plotTraces <- function(d, factorNameList = NULL)
+{
+  if (is.null(factorNameList))
+  {
+    factorNameList <- colnames(d)[2:ncol(d)];
+  }
+  arrayIndexList <- list();
+  for (arrayName in as.character(unique(d[["Array"]])))
+  {
+    arrayIndexList[[arrayName]] <- which(d[["Array"]] == arrayName);
+  }
+  arrCol <- traceColors(length(arrayIndexList));
+  names(arrCol) <- names(arrayIndexList);
+  for (factorName in factorNameList)
+  {
+    factorTrace <- d[[factorName]];
+    plot(1:length(factorTrace), factorTrace, type = "l", col = "white", main = factorName);
+    for (arrayName in names(arrayIndexList))
+    {
+      lines(arrayIndexList[[arrayName]], factorTrace[arrayIndexList[[arrayName]]], type = "l", col = arrCol[arrayName]);
+    }
+    if (readline(sprintf("%s -- q to quit: ", factorName)) == "q")
+    {
+      break;
+    }
+  }
+}
+
+
+plotArrayTraces <- function(d, factorNameList = NULL, arrayNameList = NULL)
+{
+  if (is.null(factorNameList))
+  {
+    factorNameList <- colnames(d)[2:ncol(d)];
+  }
+  if (is.null(arrayNameList))
+  {
+    arrayNameList <- as.character(unique(d[["Array"]]));
+  }
+  for (factorName in factorNameList)
+  {
+    for (arrayName in arrayNameList)
+    {
+      plot(d[d[["Array"]] == arrayName, factorName], type = "l", main = sprintf("factor %s, array %s", factorName, arrayName));
+      userQuit <- readline(sprintf("factor %s, array %s -- q to quit: ", factorName, arrayName)) == "q";
+      if (userQuit)
+      {
+        break;
+      }
+    }
+    if (userQuit)
+    {
+      break;
+    }
+  }
+}
+
+
+readSimsetExpression <- function(filename)
+{
+  d <- read.table(filename, header = TRUE, sep = "\t");
+  rownames(d) <- as.character(d[[1]]);
+  d <- d[, 2:ncol(d)];
+  return(d);
+}
--- modeldiscrimination/trunk/testkojas.sh
+++ modeldiscrimination/trunk/testkojas.sh
@@ -12,19 +12,5 @@ function do_run ()
 }
 
 
-#do_run ./transsysrandomprogram -n test -m 1 -p rtptest.dat
-#do_run mv test01.tra test_targettopology.tra
-#do_run ./transsysreparam -T transformerfile.dat -s 1 -p 1 -n test test_targettopology.tra
-#do_run mv test01.tra test_target.tra
-do_run ./transsyswritesimsetOF -o jasmonate_model_v1.txt -s 2 -N 0 jasmonate.tra test_target dummy.txt
-#do_run ./transsyswritesimsetOF -o modeldisdummy_2gene_treat.txt -s 2 -N 0.1 test_target_treat.tra test_target dummy_treat.txt
-#do_run ./transsysrewire -w 1 -n test_candidate -r 1 -s 3 test_targettopology.tra
-#do_run ./transsysrewire -w 3 -n test_candidate -r 1 -s 3 test_targettopology.tra
-#do_run ./transsysrewire -w 4 -n test_candidate -r 1 -s 3 test_targettopology.tra
-
-#do_run ./netopt -x test_target_expr.txt -o modeldisdummy.txt -R 5 -g optspec.dat -T transformerfile.dat -s 1 -c test_candidate_w04_r01.tra t1.tzxt t2.txt
-
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 5 -e 100 -n test_target -c test_targettopology -g optspec.dat -T transformerfile.dat -u correlation -L test_rew0 testlog_rew0.txt testfinalparam_rew0.txt
-#do_run ./netoptrew -l -o 1e-10 -s 4 -R 1 -e 100 -n test_target -c test_candidate_w01_r01 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 1 -e 100 -n test_target -c test_candidate_w03_r01 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
-#do_run ./netoptrew -l -o 0.01 -s 4 -R 1 -e 100 -n test_target -c test_candidate_w10_r01 -g optspec.dat -T transformerfile.dat -u correlation -L test_rew1 testlog_rew1.txt testfinalparam_rew1.txt
+# do_run ./transsyswritesimsetOF -o jasmonate_model_v1.txt -s 2 -N 0 jasmonate.tra testkojas testkojas_trace.txt
+do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate.tra testkojas testkojas_trace.txt
