fc2c1f0f267f062e226454340f4b5d83afab0086
bkx08wju <bkx08wju@7efb0120-2ad2-4840-852c-56c2cfac7ca6>
2010-09-24 12:48:41 +0000

    Jasmonate optimisation files

--- modeldiscrimination/trunk/testkojas.sh
+++ modeldiscrimination/trunk/testkojas.sh
@@ -12,6 +12,9 @@ function do_run ()
 }
 
 
-# do_run ./transsyswritesimsetOF -o jasmonate_model_v1.txt -s 2 -N 0 jasmonate.tra testkojas testkojas_trace.txt
+#do_run ./transsyswritesimsetOF -o jasmonate_model_v1.txt -s 2 -N 0 jasmonate.tra testkojas testkojas_trace.txt
 do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate.tra testkojas testkojas_trace.txt
-do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -s 1 -c jasmonate_m01.tra t1.txt t2.txt
+do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate.tra t1.txt t2.txt
+do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate_m01.tra t1.txt t2.txt
+do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate_m02.tra t1.txt t2.txt
+do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate_m03.tra t1.txt t2.txt
