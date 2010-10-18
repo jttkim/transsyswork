3892fcc33f0cf152fecd72f176272db4b710f6d4
bkx08wju <anyelacamargo@users.noreply.github.com>
2010-10-18 10:51:13 +0000

    amendment to correct for sd == 0 across array leading to exception.

--- modeldiscrimination/trunk/testkojas.sh
+++ modeldiscrimination/trunk/testkojas.sh
@@ -13,11 +13,12 @@ function do_run ()
 
 
 #do_run ./transsyswritesimsetOF -o jasmonate_model_v1.txt -s 2 -N 0 jasmonate.tra testkojas testkojas_trace.txt
-do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m00.tra jasmonatem00 testkojas_trace.txt
-do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m01.tra jasmonatem01 testkojas_trace.txt
-do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m02.tra jasmonatem02 testkojas_trace.txt
-do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m03.tra jasmonatem03 testkojas_trace.txt
+#do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m00.tra jasmonatem00 testkojas_trace.txt
+#do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m01.tra jasmonatem01 testkojas_trace.txt
+#do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m02.tra jasmonatem02 testkojas_trace.txt
+#do_run ./transsyswritesimsetOF -o jasmonate_model_snapshot.txt -s 2 -N 0 jasmonate_m03.tra jasmonatem03 testkojas_trace.txt
 #do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate.tra t1.txt t2.txt
+do_run ./netopt -x test_target_expr_temp01.txt -o simgenex_temp01.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c test_temp01 t1.txt t2.txt
 #do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate_m01.tra t1.txt t2.txt
 #do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate_m02.tra t1.txt t2.txt
 #do_run ./netopt -x testkojas_expr.txt -o jasmonate_model_snapshot.txt -R 5 -g optspec.dat -T transformerfile.dat -L logo -s 1 -c jasmonate_m03.tra t1.txt t2.txt
