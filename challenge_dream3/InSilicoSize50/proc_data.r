5ecfad1839cbe2e512d266c71fc56b86b75f8d9d
bkx08wju <anyelacamargo@users.noreply.github.com>
2011-06-10 18:20:34 +0000

    Please can you check the GoldStandar transsys network?

---
+++ challenge_dream3/InSilicoSize50/proc_data.r
@@ -0,0 +1,12 @@
+processdata <- function(data)
+{
+  for(i in 1:50)
+  {
+    rname <- paste("G", i, "(+/-)", sep="");
+    t <- which(rownames(data) == rname);
+    rownames(data)[t] <- paste("G", i, "KO", sep="");
+  }
+
+  data <- t(data);
+  write.table(data, file="procdata.tgs", quote=FALSE);
+}
