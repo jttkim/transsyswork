057680c01709c0d3f818081be55da1d617c05d7c
Jan T. Kim <jttkim@users.noreply.github.com>
2011-10-18 13:43:35 +0000

    commit in preparation for move to dreamhost SVN

--- challenge_dream3/InSilicoSize50/proc_data.r
+++ challenge_dream3/InSilicoSize50/proc_data.r
@@ -9,4 +9,17 @@ processdata <- function(data)
 
   data <- t(data);
   write.table(data, file="procdata.tsv", sep="\t", quote=FALSE);
+  invisible(data);
 }
+
+
+readGoldStandardData <- function(filename)
+{
+  d <- read.table(filename, header = TRUE, sep = "\t");
+  rownames(d) <- as.character(d[[1]]);
+  d <- d[, 2:ncol(d)];
+  return(d);
+}
+
+gsd <- readGoldStandardData("InSilicoSize50-Ecoli1-null-mutants.tsv");
+processdata(gsd);
